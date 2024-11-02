2717b6f61e38c96c7c6f3f493c593fd784951500
Adnan Temur Barcha <53247461+AdnanTemur@users.noreply.github.com>
2024-11-03 03:07:02 +0500

    Update BentoGrid.jsx

--- components/ui/BentoGrid.jsx
+++ components/ui/BentoGrid.jsx
@@ -55,7 +55,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "johndoe@somemail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
